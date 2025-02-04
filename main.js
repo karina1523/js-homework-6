dd03df5060e8cb0da221ea254d74c57a86baa2e7
wert@gmail.com <karina1223@gmail.com>
2025-02-04 19:09:57 +0500

    js homework-6

---
+++ main.js
@@ -0,0 +1,39 @@
+let com = []
+for (let i = 0; i < Infinity; i++) {
+    let text = prompt('Введите команду (add/del/stop)');
+    if (text.startsWith('add, ')) {
+        let item = text.replace('add, ', "")
+        com.push(item);
+        console.log(item);
+        console.log(com);
+
+    } else if (text.startsWith('del,')) {
+        let remoted = text.replace('del, ', '');
+        let index = com.indexOf(remoted);
+        if (index !== -1) {
+            com.splice(index, 1);
+            console.log(index);
+            console.log(com);
+            
+
+        } else {
+            console.log(com);
+        }
+
+    } else if (text === 'stop') {
+        break;
+    }
+}
+console.log(com);
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+let odd = []
+let even = []
+for (let num of array) {
+    if (num % 2 == 0) {
+        even.push(num);
+    } else {
+        odd.push(num);
+    }
+}
